6318ad849304f91b3f80612bda914e2f3b06090f
SpideyFanDan <dsmith.teach@gmail.com>
2021-09-03 13:40:09 -0600

    added get requests for all posts and individual posts in API

--- spidey-blog-api/api/models/posts.js
+++ spidey-blog-api/api/models/posts.js
@@ -5,16 +5,26 @@ class Post {
 	get() {
 		return this.readData();
 	}
-	getIndividualBlog() {
+	getIndividualBlog(postId) {
 		//get individual blog post
+		const posts = this.readData();
+		const foundPost = posts.find((post) => post.id == postId);
+		return foundPost;
 	}
-	add() {
+	add(newPost) {
 		//add new posts
+		const currentPosts = this.readData();
+		currentPosts.unshift(newPost);
+		this.storeData(currentPosts);
 	}
 	readData() {
 		let rawdata = fs.readFileSync(PATH);
 		let posts = JSON.parse(rawdata);
 		return posts;
+	}
+	storeData(rawData) {
+		let data = JSON.stringify(rawData);
+		fs.writeFileSync(PATH, data);
 	}
 }
 module.exports = Post;
