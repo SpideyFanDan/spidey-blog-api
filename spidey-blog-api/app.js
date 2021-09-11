17c64ec3a2aaa60d863741a11c25f72f97868322
SpideyFanDan <dsmith.teach@gmail.com>
2021-09-10 20:30:55 -0600

    completed new post functionality for blog api

--- spidey-blog-api/app.js
+++ spidey-blog-api/app.js
@@ -2,6 +2,26 @@ const express = require('express');
 const app = express();
 const Post = require('./api/models/posts');
 const postsData = new Post();
+const multer = require('multer');
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, '/uploads');
+	},
+	filename: function (req, file, cb) {
+		cb(null, `${file.fieldname}-${Date.now()}${getExt(file.mimeType)}`);
+	},
+});
+const getExt = (mimeType) => {
+	switch (mimeType) {
+		case 'image/png':
+			return '.png';
+		case 'image/jpeg':
+			return '.jpeg';
+	}
+};
+const upload = multer({ storage: storage });
+
+app.use(express.json());
 
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
@@ -17,6 +37,19 @@ app.get('/api/posts/:post_id', (req, res) => {
 	if (foundPost) {
 		res.status(200).send(foundPost);
 	} else res.status(404).send('Not found');
+});
+app.post('/api/posts', upload.single('image'), (req, res) => {
+	const newPost = {
+		id: `${Date.now()}`,
+		title: req.body.title,
+		category: req.body.category,
+		location: req.body.location,
+		content: req.body.content,
+		image: req.file.path,
+		date_added: req.body['date_added'],
+	};
+	postsData.add(newPost);
+	res.status(200).send(newPost);
 });
 
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
